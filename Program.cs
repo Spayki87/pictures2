7bb29d6f47a126515ca68329dd041fd0163c9e41
Spayki87 <136745298+Spayki87@users.noreply.github.com>
2023-06-19 14:40:51 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Вывод_картинок
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int picturesInRow = 3;
+            int pictures = 52;
+            int filledRows = pictures / picturesInRow;
+            Console.WriteLine("У вас получилось " + filledRows + " заполненых рядов.");
+            int remainingPictures = pictures % picturesInRow;
+            Console.WriteLine("У вас не влезла " + remainingPictures + " картинок.");
+        }
+    }
+}
